3f6dd924744d109732db6de8468e8cfe833fc9bb
Anshum Shukla <113186411+AnshumShukla@users.noreply.github.com>
2023-04-13 11:58:46 +0530

    initialedrrepo.

---
+++ Java/divide.java
@@ -0,0 +1,8 @@
+public class divide {
+    public static void main(String[] args) {
+        int  a = 70;
+        int  b = 70;
+        int divide = a /b;
+        System.out.println(divide);
+    }
+}
